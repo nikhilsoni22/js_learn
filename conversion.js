7ca85bd4a3048b254a0ee70299255a1cad2ce8d8
nikhil <nikhilsoni4121@gmail.com>
2025-01-23 01:17:17 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,29 @@
+/* let score = "nikhil"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+console.log(valueInNumber) */
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+
+/* let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) */
+
+//1 => true
+//0 => false
+//"" => false
+//null => false
+//undefined => false
+//"23" => true
+
+let someNumber = 23
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber)
