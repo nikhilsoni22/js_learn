0a208099f2d45f6d685a79f035c7af73da412050
nikhil <nikhilsoni4121@gmail.com>
2025-01-22 15:33:56 +0530

    2nd commit

---
+++ 1_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123456
+let accountEmail = "nikhil@gmail.com"
+var  accountPassword = "123456"
+accountCity = "jaipur"
+
+let accountState;
+
+//accountId = 2
+
+accountEmail = "nik@gmail.com"
+accountPassword = "21212121"
+accountCity = "delhi"
+
+console.log(accountId);
+console.table([
+    accountId, accountEmail, accountPassword, accountCity, accountState
+])
---
+++ 2.datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+//alert(3 + 3) we are using node.js not browser
+
+console.log(3 + 3)
+
+console.log("hitest")
+
+let name = "nikhil"
+let age = 22
+let isLoggedIn = false
+
+//number => to power 53
+//bigInt
+//String = ""
+//boolean = true/false
+// null = standlone value
+//undefined = standlone value
+//object = {}
+//symbol = unique value
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
